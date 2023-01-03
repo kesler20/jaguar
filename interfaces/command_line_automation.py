9fc267989422eb81622482abd00afa0e0af1a4d7
kesler20 <uchekesla@gmail.com>
2023-01-03 05:04:16 +0000

    documentation: update the commands.txt📰

--- interfaces/command_line_automation.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-os.system("mkdir test_dir")
-os.system("cd test_dir")
-os.system("git clone https://github.com/kesler20/interfaces")
-os.system("cd ..")
-os.system("rmdir -/S test_dir")
